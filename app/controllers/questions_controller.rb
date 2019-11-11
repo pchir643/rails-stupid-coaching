4b18004309dc757ef8940b0e1a6438f46111ca63
Philippe CHIRIE <p.chirie@laposte.net>
2019-11-11 15:01:21 +0100

    2me validation

---
+++ app/controllers/questions_controller.rb
@@ -0,0 +1,14 @@
+class QuestionsController < ApplicationController
+  def ask
+  end
+
+  def answer
+    if params[:question] == "I am going to work"
+      @message = "Great!"
+    elsif params[:question].last == "?"
+      @message = "Silly question"
+    else
+      @message = "I don't care, get dressed and go to work!"
+    end
+  end
+end
